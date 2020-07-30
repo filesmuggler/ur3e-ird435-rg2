8ae75fa18329ff160689fabf6fbd0f366942e12d
filesmuggler <krzste09@outlook.com>
2020-07-30 16:29:36 +0200

    moveit package added

---
+++ catkin_ws/build/ur3e_d435_rg2_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/krzysztof/Repos/ur3e-ird435-rg2/catkin_ws/src/ur3e_d435_rg2_moveit
+# Build directory: /home/krzysztof/Repos/ur3e-ird435-rg2/catkin_ws/build/ur3e_d435_rg2_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
