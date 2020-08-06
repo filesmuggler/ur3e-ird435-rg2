736a78a0efd15f6cefa73ae099c0f1c05e09d1d8
filesmuggler <krzste09@outlook.com>
2020-08-06 20:27:33 +0200

    d435 on arm giving depth

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ur3e-ird435-rg2
 Object detection with Intel RealSense D435 and object manipulation with UR3e arm with OnRobot RG2
+
+roslaunch ur_robot_driver ur3e_bringup_bunkier.launch robot_ip:=150.254.47.230   kinematics_config:=$(rospack find ur_calibration)/etc/ur3e_calibration_bunkier.yaml
+
+roslaunch ur3e_d435_rg2_moveit ur3e_d435_rg2_moveit_planning_execution.launch
+
---
+++ catkin_ws/build/realsense_nodes_python/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/krzysztof/Repos/ur3e-ird435-rg2/catkin_ws/src/realsense_nodes_python
+# Build directory: /home/krzysztof/Repos/ur3e-ird435-rg2/catkin_ws/build/realsense_nodes_python
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
