19e67abaeffdc982b5dc72742db211b3596cf4bb
filesmuggler <krzste09@outlook.com>
2020-08-12 11:33:26 +0200

    b4 ros industrial

---
+++ catkin_ws/build/ur_rg2_gripper/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/krzysztof/Repos/ur3e-ird435-rg2/catkin_ws/src/ur_rg2_gripper
+# Build directory: /home/krzysztof/Repos/ur3e-ird435-rg2/catkin_ws/build/ur_rg2_gripper
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
