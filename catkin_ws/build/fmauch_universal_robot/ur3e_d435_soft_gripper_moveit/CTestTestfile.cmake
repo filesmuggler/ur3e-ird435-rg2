06187699993da6f87c41f2d68025fdfefe6e3397
filesmuggler <krzste09@outlook.com>
2020-08-14 14:01:56 +0200

    I like to moveIT

---
+++ catkin_ws/build/fmauch_universal_robot/ur3e_d435_soft_gripper_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/krzysztof/Repos/ur3e-ird435-rg2/catkin_ws/src/fmauch_universal_robot/ur3e_d435_soft_gripper_moveit
+# Build directory: /home/krzysztof/Repos/ur3e-ird435-rg2/catkin_ws/build/fmauch_universal_robot/ur3e_d435_soft_gripper_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
